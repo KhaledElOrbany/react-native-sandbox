2a05510c8d0a4dfbb3e34eee1edbee8cca5742fb
elorbany <khaled.wagdy554@gmail.com>
2023-11-11 23:26:45 +0300

    Update App.jsx to use BottomSheetRoot instead of ActionMenu

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import ActionMenu from './components/ActionMenu';
-// import BottomSheetRoot from './modules/BottomSheet';
+// import ActionMenu from './components/ActionMenu';
+import BottomSheetRoot from './modules/BottomSheet';
 
 export default function App() {
   return (
@@ -12,8 +12,8 @@ export default function App() {
         backgroundColor="transparent"
       />
 
-      {/* <BottomSheetRoot /> */}
-      <ActionMenu />
+      <BottomSheetRoot />
+      {/* <ActionMenu /> */}
     </>
   );
 }
